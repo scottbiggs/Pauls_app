e11f877e397ecd667d0322923f1672aa9ae38467
sbiggs <scott.biggs@consensus-builder.com>
2024-10-15 13:52:46 -0500

    starting lots of changes to get the philips hue stuff going. added artwork for how to find the bridge ip.

--- app/src/main/java/com/sleepfuriously/paulsapp/MainActivity.kt
+++ app/src/main/java/com/sleepfuriously/paulsapp/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.sleepfuriously.paulsapp
 
+import android.content.res.Configuration
+import android.content.res.Configuration.ORIENTATION_LANDSCAPE
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sleepfuriously.paulsapp.ui.theme.PaulsAppTheme
-import com.sleepfuriously.paulsapp.view.ShowMainScreen
 
 
 /**
@@ -36,7 +58,8 @@ class MainActivity : ComponentActivity() {
     //----------------------------
 
     /** access to the view model */
-    private val viewModel by viewModels<MainViewModel>()
+    private lateinit var viewModel: MainViewModel
+
 
     //----------------------------
     //  functions
@@ -45,6 +68,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        viewModel  = MainViewModel(applicationContext)
+
         setContent {
             PaulsAppTheme {
 
@@ -57,7 +83,198 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+
+    //----------------------------
+    //  composables
+    //----------------------------
+
+    /**
+     * The main screen that the user interacts with.  All the big
+     * stuff goes here.
+     */
+    @Composable
+    fun ShowMainScreen(
+        modifier : Modifier = Modifier,
+        viewModel: MainViewModel
+    ) {
+        // useful to know!
+        val config = LocalConfiguration.current
+        val screenHeight = config.screenHeightDp.dp
+        val screenWidth = config.screenWidthDp.dp
+        val landscape = config.orientation == ORIENTATION_LANDSCAPE
+
+        var splashScreenDone by remember { mutableStateOf(true) }
+
+        if (viewModel.bridgeInit == PhilipsHueBridgeInit.BRIDGE_INITIALIZING) {
+            splashScreenDone = false
+        }
+
+/*
+        AnimatedVisibility(
+            visible = viewModel.bridgeInit == PhilipsHueBridgeInit.INITIALIZING,
+            enter = fadeIn(animationSpec = tween(2000)),
+
+            // exit by sliding left and fading
+            exit = slideOutHorizontally(
+                targetOffsetX = { fullWidth ->
+                    -fullWidth
+                },
+                animationSpec = tween(1000)
+            ) // + fadeOut(targetAlpha = 0f, animationSpec = tween(1000))
+        ) {
+            SplashScreen()
+        }
+*/
+        when (viewModel.bridgeInit) {
+
+            PhilipsHueBridgeInit.BRIDGE_UNINITIALIZED -> {
+                DisplayInitPHBridgeButton(viewModel, modifier)
+            }
+
+            PhilipsHueBridgeInit.BRIDGE_INITIALIZING -> {
+            }
+
+            PhilipsHueBridgeInit.BRIDGE_INITIALIZED -> {
+                // todo
+                Toast.makeText(LocalContext.current, stringResource(R.string.bridge_found), Toast.LENGTH_LONG).show()
+
+                if (splashScreenDone) {
+                    Column(modifier = Modifier.fillMaxSize(), Arrangement.Center) {
+                        Text(
+                            "Hi scott!  it's looking good so far.",
+                            modifier = Modifier
+                                .align(alignment = Alignment.CenterHorizontally),
+                        )
+                    }
+                }
+            }
+
+            PhilipsHueBridgeInit.BRIDGE_INITIALIZATION_TIMEOUT -> {
+                // todo
+                Text(stringResource(R.string.cannot_find_bridge))
+            }
+
+            PhilipsHueBridgeInit.ERROR -> {
+                // todo
+                Text(stringResource(R.string.error_init_bridge))
+            }
+        }
+
+    }
+
+    /**
+     * Displays the content of the splash screen.  Animations should be
+     * done at the caller level.
+     */
+    @Composable
+    private fun SplashScreen() {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(80.dp)
+//            .background(color = Color(red = 0xb0, green = 0xc0, blue = 0xe0)),
+                .background(color = MaterialTheme.colorScheme.primary),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(id = R.string.initializing),
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+            CircularProgressIndicator()
+        }
+    }
+
+    /**
+     * Simply shows the button that starts the initialization process.
+     * This will change the state such that a screen will show the user
+     * what to do.
+     */
+    @Composable
+    private fun DisplayInitPHBridgeButton(
+        viewModel: MainViewModel,
+        modifier: Modifier = Modifier
+    ) {
+
+        Box(modifier = modifier.fillMaxSize()) {
+
+            Button(onClick = {
+                viewModel.startManualBridgeInit()
+            }) {
+                Text(stringResource(id = R.string.start_ph_bridge_init_button))
+            }
+        }
+    }
+
+    /**
+     * Initializing the Philips Hue bridge, part 1.
+     * The user simply has to fill in the IP of the bridge.
+     * Information on how to do it is also displayed.
+     */
+    @Composable
+    private fun DisplayManualInitPart1() {
+        var ipText by remember { mutableStateOf("") }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colorScheme.secondary)
+        ) {
+
+            Text(
+                stringResource(id = R.string.enter_ip_hint),
+                color = MaterialTheme.colorScheme.onSecondary
+            )
+            OutlinedTextField(
+                value = ipText,
+                label = { Text(stringResource(id = R.string.enter_ip)) },
+                singleLine = true,
+                onValueChange = { ipText = it }
+            )
+        }
+
+    }
+
+
+    /**
+     * This requires the user to hit the big button on the Philips
+     * Hue bridge and THEN hit the go button on their device.  This
+     * starts the sequence of the bridge sending back the secret
+     * name for this app to use.
+     */
+    @Composable
+    private fun DisplayManualInitPart2() {
+
+    }
+
+    //----------------------------
+    //  previews
+    //----------------------------
+
+    @Preview(name = "init part 1", uiMode = Configuration.UI_MODE_NIGHT_YES)
+    @Composable
+    private fun DisplayManualInitPart1Preview() {
+        DisplayManualInitPart1()
+    }
+
+/*
+    @Preview(
+        name = "tablet",
+        device = "spec:shape=Normal,width=1240,height=640,unit=dp,dpi=480",
+        showSystemUi = true,
+        showBackground = true
+    )
+    @Composable
+    private fun ShowMainScreenPreview() {
+        PaulsAppTheme {
+            ShowMainScreen(viewModel = viewModel)
+        }
+    }
+*/
 }
+
+
 
 
 //----------------------------
--- app/src/main/java/com/sleepfuriously/paulsapp/MainViewModel.kt
+++ app/src/main/java/com/sleepfuriously/paulsapp/MainViewModel.kt
@@ -1,10 +1,15 @@
 package com.sleepfuriously.paulsapp
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -14,35 +19,119 @@ import kotlinx.coroutines.launch
  * main screen.  The parts of the screen should have their
  * own viewmodels.
  */
-class MainViewModel : ViewModel() {
+class MainViewModel(ctx: Context) : ViewModel() {
 
+    /** Tells the UI which state to display */
+    var displayStates by mutableStateOf(MainActivityDisplayStates.UNKNOWN)
+        private set
 
     /** communicates state of bridge initialization to the ui */
-    var bridgeInit by mutableStateOf(PhilipsHueBridgeInit.INITIALIZING)
+    var bridgeInit by mutableStateOf(PhilipsHueBridgeInit.BRIDGE_INITIALIZING)
         private set
+
 
     /**
      * todo:  this is for testing the initialization graphic
      */
     init {
         viewModelScope.launch {
+
+            val epref = getESharedPref(ctx) as EncryptedSharedPreferences
+
+            val ph_name = epref.getString(PHILIPS_HUE_NAME_KEY, PH_NAME_NOT_FOUND)
+            if (ph_name.equals(PH_NAME_NOT_FOUND)) {
+                bridgeInit = PhilipsHueBridgeInit.BRIDGE_UNINITIALIZED
+                Log.v(TAG, "Philips Hue Bridge not initialized")
+                return@launch
+            }
+
+
             delay(3000)
-            bridgeInit = PhilipsHueBridgeInit.INITIALIZED
+            bridgeInit = PhilipsHueBridgeInit.BRIDGE_INITIALIZED
         }
     }
 
+
+    /**
+     * Gets a reference to an encrypted shared prefs file.  I presume that
+     * if it doesn't exist, a new one will be created.  Should otherwise
+     * work just like a regular prefs file.
+     *
+     * from:  https://stackoverflow.com/a/64115229/624814
+     */
+    private fun getESharedPref(ctx: Context) : SharedPreferences {
+        val masterKey = MasterKey.Builder(ctx)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        return EncryptedSharedPreferences.create(
+            ctx,
+            SHARED_PREFS_FILENAME,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+
+
+    /**
+     * The function that starts the guided tour that is the manual
+     * Philips Hue bridge setup.
+     *
+     * side effects:
+     */
+    fun startManualBridgeInit() {
+
+    }
+
+}
+
+/**
+ * Display states
+ */
+enum class MainActivityDisplayStates {
+    /** The app is going through its startup sequence.  Show splash screen. */
+    APP_STARTUP,
+    /** Initialization process has begun.  Show init stuff.  todo: May need subivisions */
+    INITIALIZING,
+    /** The normal running state of the app.  All data is displayed and accessible. */
+    RUNNING,
+    /** When an error occurs, this indicates the error condition. todo: more error states needed */
+    ERROR,
+    /** When the app complete starts we're in an unknown state. */
+    UNKNOWN
 }
 
 /**
  * The states of the Philips Hue bridge.
  */
 enum class PhilipsHueBridgeInit {
+
+    /** the bridge has not been initialized yet */
+    BRIDGE_UNINITIALIZED,
     /** currently attempting to initialize the bridge */
-    INITIALIZING,
+    BRIDGE_INITIALIZING,
     /** attempt to initialize the bridge has timed out */
-    INITIALIZATION_TIMEOUT,
+    BRIDGE_INITIALIZATION_TIMEOUT,
     /** successfully initialized */
-    INITIALIZED,
+    BRIDGE_INITIALIZED,
     /** some error has occurred when dealing with the bridge */
     ERROR
 }
+
+private const val TAG = "MainViewModel"
+
+/** Name of the encrypted shared prefs file */
+private const val SHARED_PREFS_FILENAME = "prefs"
+
+/**
+ * Key for preferences that holds the secret name to access the
+ * philips hue bridge for this app.
+ */
+private const val PHILIPS_HUE_NAME_KEY = "ph_name_key"
+
+/**
+ * If the philips hue bridge access name is this, that means that the
+ * name hasn't been created yet.  Time to initialize the PH bridge!
+ */
+private const val PH_NAME_NOT_FOUND = "not_found"
