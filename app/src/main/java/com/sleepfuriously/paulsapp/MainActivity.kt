9d904fc26234c3d9a946b7d2b94a37e9e89ee924
sbiggs <scottmorganbiggs@gmail.com>
2025-12-26 18:14:12 -0600

    cleaning up MainActivity.kt.  I don't think that the splash screen needs to be referenced here anymore.

--- app/src/main/java/com/sleepfuriously/paulsapp/MainActivity.kt
+++ app/src/main/java/com/sleepfuriously/paulsapp/MainActivity.kt
@@ -1,18 +1,12 @@
 package com.sleepfuriously.paulsapp
 
-import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.view.animation.OvershootInterpolator
-import android.widget.Spinner
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,53 +15,46 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import androidx.core.animation.doOnEnd
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.sleepfuriously.paulsapp.compose.philipshue.ManualBridgeSetup
-import com.sleepfuriously.paulsapp.compose.philipshue.ShowMainScreenPhilipsHue
-import com.sleepfuriously.paulsapp.compose.philipshue.ShowScenesForRoom
 import com.sleepfuriously.paulsapp.compose.SimpleFullScreenBoxMessage
+import com.sleepfuriously.paulsapp.compose.philipshue.ManualBridgeSetup
 import com.sleepfuriously.paulsapp.compose.philipshue.ShowAddOrEditFlockDialog
+import com.sleepfuriously.paulsapp.compose.philipshue.ShowMainScreenPhilipsHue
 import com.sleepfuriously.paulsapp.compose.philipshue.ShowScenesForFlock
+import com.sleepfuriously.paulsapp.compose.philipshue.ShowScenesForRoom
 import com.sleepfuriously.paulsapp.compose.philipshue.ShowScenesForZone
+import com.sleepfuriously.paulsapp.compose.sprinkler.ShowMainSprinkler
 import com.sleepfuriously.paulsapp.model.philipshue.data.PhilipsHueBridgeInfo
 import com.sleepfuriously.paulsapp.model.philipshue.data.PhilipsHueFlockInfo
 import com.sleepfuriously.paulsapp.ui.theme.PaulsAppTheme
+import com.sleepfuriously.paulsapp.ui.theme.coolGray
+import com.sleepfuriously.paulsapp.ui.theme.lightBlueDark
 import com.sleepfuriously.paulsapp.viewmodels.BridgeInitStates
+import com.sleepfuriously.paulsapp.viewmodels.ClimateViewmodel
 import com.sleepfuriously.paulsapp.viewmodels.MainViewmodel
+import com.sleepfuriously.paulsapp.viewmodels.MyViewModelInterface
 import com.sleepfuriously.paulsapp.viewmodels.PhilipsHueViewmodel
+import com.sleepfuriously.paulsapp.viewmodels.PoolViewmodel
 import com.sleepfuriously.paulsapp.viewmodels.SceneDataForFlock
 import com.sleepfuriously.paulsapp.viewmodels.SceneDataForRoom
 import com.sleepfuriously.paulsapp.viewmodels.SceneDataForZone
-import com.sleepfuriously.paulsapp.viewmodels.TestStatus
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.unit.sp
-import com.sleepfuriously.paulsapp.compose.sprinkler.ShowMainSprinkler
-import com.sleepfuriously.paulsapp.ui.theme.coolGray
-import com.sleepfuriously.paulsapp.ui.theme.darkBlueLight
-import com.sleepfuriously.paulsapp.ui.theme.lightBlueDark
-import com.sleepfuriously.paulsapp.ui.theme.lightBlueMain
-import com.sleepfuriously.paulsapp.viewmodels.ClimateViewmodel
-import com.sleepfuriously.paulsapp.viewmodels.MyViewModelInterface
-import com.sleepfuriously.paulsapp.viewmodels.PoolViewmodel
 import com.sleepfuriously.paulsapp.viewmodels.SecurityViewmodel
 import com.sleepfuriously.paulsapp.viewmodels.SprinklerViewmodel
+import com.sleepfuriously.paulsapp.viewmodels.TestStatus
 
 
 /**
@@ -123,10 +110,9 @@ class MainActivity : ComponentActivity() {
 
         setContent {
 
-            // start initializations and splash screen
+            // start initializations
             mainViewmodel.initialize(this)
             philipsHueViewmodel.initialize()
-//                    showSplashScreen()
 
             PaulsAppTheme {
 
@@ -140,7 +126,6 @@ class MainActivity : ComponentActivity() {
 
                 val phTestingState by philipsHueViewmodel.phTestingState.collectAsStateWithLifecycle()
                 val phTestingErrorMsg by philipsHueViewmodel.phTestingErrorMsg.collectAsStateWithLifecycle()
-                val philipsHueTestStatus by philipsHueViewmodel.philipsHueTestStatus.collectAsStateWithLifecycle()
                 val addNewBridgeState by philipsHueViewmodel.addNewBridgeState.collectAsStateWithLifecycle()
                 val philipsHueFinishNow by philipsHueViewmodel.crashNow.collectAsStateWithLifecycle()
                 val showWaitSpinner by philipsHueViewmodel.waitingForResponse.collectAsStateWithLifecycle()
@@ -180,7 +165,6 @@ class MainActivity : ComponentActivity() {
                         Log.d(TAG, "   philipsHueIntializing = $philipsHueInitializing")
 
                         ShowInitializingScreen(Modifier.padding(innerPadding))
-//                        showSplashScreen()
                     }
 
                     else if (wifiWorking == false) {
@@ -479,34 +463,6 @@ class MainActivity : ComponentActivity() {
 
 
     /**
-     * Displays the content of the splash screen.  Animations should be
-     * done at the caller level.
-     */
-    @Composable
-    private fun SplashScreenContents(modifier: Modifier = Modifier) {
-
-        Log.d(TAG, "SplashScreenContents()")
-
-        Box(modifier = modifier) {
-            Column(
-                modifier = modifier
-                    .fillMaxSize()
-                    .padding(50.dp)
-                    .background(color = MaterialTheme.colorScheme.tertiary),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = stringResource(id = R.string.initializing),
-                    color = MaterialTheme.colorScheme.onTertiary
-                )
-                CircularProgressIndicator()
-            }
-        }
-    }
-
-
-    /**
      * Begins the process of manually initializing the IoT devices.
      */
     @Composable
@@ -539,57 +495,6 @@ class MainActivity : ComponentActivity() {
             buttonText = stringResource(id = R.string.exit),
             onClick = { finish() }
         )
-    }
-
-
-    /**
-     * Handles displaying the splash screen.  Needs to be
-     * called BEFORE setContent() in [onCreate].
-     *
-     * preconditions:
-     *      philipsHueViewmodel       ready to use
-     */
-    private fun showSplashScreen() {
-        // Needs to be called before setContent() or setContentView().
-        // apply{} is optional--use to do any additional work.
-        installSplashScreen().apply {
-            // example: this will check the value every frame and keep showing
-            //          the splash screen as long as the total value is true
-            setKeepOnScreenCondition {
-                (philipsHueViewmodel.phTestingState.value == TestStatus.TESTING) // ||
-//                (phTestingState == TestStatus.TESTING) ||
-//                 philipsHueInitializing
-            }
-
-            // set the exit animation
-            setOnExitAnimationListener { screen ->
-                val zoomX = ObjectAnimator.ofFloat(
-                    screen.iconView,
-                    View.SCALE_X,
-                    0.7f,       // start will be the final value in logo_animator.xml
-                    8.0f                // what we'll finish with (expand to entire width of screen)
-                )
-                zoomX.interpolator = OvershootInterpolator(0f)    // provides a bump when animating (2f is default)
-                zoomX.duration = 750L      // duration of exit animation
-                zoomX.doOnEnd { screen.remove() }   // when done, remove this screen and go on to next
-
-                // same for y value
-                val zoomY = ObjectAnimator.ofFloat(
-                    screen.iconView,
-                    View.SCALE_Y,
-                    0.7f,
-                    0.0f                // go to nothing
-                )
-                zoomY.interpolator = OvershootInterpolator(0f)
-                zoomY.duration = 750L
-                zoomY.doOnEnd { screen.remove() }
-
-                // finally start the animations
-                zoomX.start()
-                zoomY.start()
-            }
-        }
-
     }
 
 
